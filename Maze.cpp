41749039c6175e236e1e8d85a280efc2212fb5d2
Vitor Ota <ota.vitor@gmail.com>
2017-08-23 21:03:14 -0300

    dude, this is harder that I thinked

--- Maze.cpp
+++ Maze.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <iostream>
+#include <stdlib.h> /* srand, rand */
+#include <time.h> /* time */
 
 using namespace std;
 
@@ -21,10 +21,11 @@ const int toLeft = 2;
 const int toUp = 3;
 
 //config
-const int mazeSize = 10;
+const int mazeSize = 11;
 
 bool randomSeedInitialized = false;
-int generateRandom(int max) {
+int generateRandom(int max)
+{
     if (!randomSeedInitialized) {
         srand(time(NULL));
         randomSeedInitialized = true;
@@ -33,42 +34,57 @@ int generateRandom(int max) {
     return rand() % max;
 }
 
-bool isPossibleMoveTo(int maze[mazeSize][mazeSize], int moveTo, int currentColumn, int currentRow){
+void printMaze(int maze[mazeSize][mazeSize])
+{
+	cout<<"----------"<<endl;
+    for (int column = 0; column < 10; column++) {
+        for (int row = 0; row < 10; row++) {
+            int field = maze[column][row];
+            cout << (field == wall ? wallChar : field == player? playerChar : emptyChar);
+        }
+        cout << endl;
+    }
+	cout<<"----------"<<endl;
+}
+
+bool isPossibleMoveTo(int maze[mazeSize][mazeSize], int moveTo, int currentColumn, int currentRow)
+{
     bool isPossibleMoveTo = true;
 
-//TODO add the check to see if the adjacents positions are wall's
+    //TODO add the check to see if the adjacents positions are walls
 
-        switch (moveTo) {
-		case toRight:
-		    isPossibleMoveTo = currentColumn < mazeSize - 1 || maze[currentColumn + 1][currentRow] != wall ;
-		    break;
-		case toDown:
-		    isPossibleMoveTo = currentRow < mazeSize - 1  || maze[currentColumn][currentRow + 1] != wall;
-		    break;
-		case toLeft:
-		    isPossibleMoveTo = currentColumn > 0  || maze[currentColumn - 1][currentRow] != wall;
-		    break;
-		case toUp:
-		    isPossibleMoveTo = currentRow > 0 || maze[currentColumn][currentRow - 1] != wall;
-		    break;
-	}
+    switch (moveTo) {
+    case toRight:
+        isPossibleMoveTo = currentColumn < mazeSize - 1 && maze[currentColumn + 1][currentRow] != wall;
+        break;
+    case toDown:
+        isPossibleMoveTo = currentRow < mazeSize - 1 && maze[currentColumn][currentRow + 1] != wall;
+        break;
+    case toLeft:
+        isPossibleMoveTo = currentColumn > 0 && maze[currentColumn - 1][currentRow] != wall;
+        break;
+    case toUp:
+        isPossibleMoveTo = currentRow > 0 && maze[currentColumn][currentRow - 1] != wall;
+        break;
+    }
 
     return isPossibleMoveTo;
 }
 
-int randomNextMove(int lastMove, int currentColumn, int currentRow) {
+int randomNextMove(int maze[mazeSize][mazeSize], int lastMove, int currentColumn, int currentRow)
+{
     //right or down ++ // left or up --
     int moveTo = -1;
     bool canMove = true;
 
     do {
         //4 possible movements
-        moveTo = generateRandom(4);
+        moveTo = generateRandom(2); //I see a bug here when moving up //TODO fix this bug
 
         // after a move, you CAN'T move to his oposite direction (example, go to down, after up)
         bool isOpositeMove = (moveTo == lastMove + (lastMove > 1 ? -2 : 2));
 
-        canMove = !isOpositeMove && isPossibleMoveTo(maze,moveTo, currentColumn,currentRow);
+        canMove = !isOpositeMove && isPossibleMoveTo(maze, moveTo, currentColumn, currentRow);
 
     } while (!canMove);
 
@@ -76,38 +92,35 @@ int randomNextMove(int lastMove, int currentColumn, int currentRow) {
 }
 
 //TODO see if pass currentColumn and currentRow as a reference is better (if positive, alter his values in the end of the function and remove the param "resultMove[2]")
-void moveOnMaze(int maze[mazeSize][mazeSize], int moveTo, int field, int& currentColumn, int& currentRow){
-	//TODO implement
-//resultMove is an array that returns the column([0]) and the row([1])
-//field is the char to print on the desired move; 
+void moveOnMaze(int maze[mazeSize][mazeSize], int moveTo, int field, int& currentColumn, int& currentRow)
+{
+    if (!isPossibleMoveTo(maze, moveTo, currentColumn, currentRow)) {
+        return;
+    }
 
-	if(!isPossibleMoveTo(maze, moveTo, currentColumn, currentRow)){
-		return;
-	}
-	
-	
-        switch (moveTo) {
+    switch (moveTo) {
 		case toRight:
-		    currentColumn++;
-		    break;
+			currentColumn++;
+			break;
 		case toDown:
-		    currentRow++;
-		    break;
+			currentRow++;
+			break;
 		case toLeft:
-		    currentColumn-;
-		    break;
+			currentColumn-- ;
+			break;
 		case toUp:
-		    currentRow--;
-		    break;
-	}
-	maze[currentColumn][currentRow] = field;
+			currentRow--;
+			break;
+    }
 	
+    maze[currentColumn][currentRow] = field;
 }
 
-void generateMaze(int maze[mazeSize][mazeSize]) {
-    //TODO see how a int array is preffiled when first initialized
+void generateMaze(int maze[mazeSize][mazeSize])
+{
+	//TODO to generate a maze I need see if I'll have escape after
+	//(possible solution: see if the next 3 movements before make a move are free)
 
-    int maze[mazeSize][mazeSize];
     int lastMove = -1; // 0 - right // 1 - down // 2 - left // 3 - up
 
     int markAsGenerated = 99999999;
@@ -115,42 +128,36 @@ void generateMaze(int maze[mazeSize][mazeSize]) {
     int currentColumn = 0;
     int currentRow = 0;
 
-    while (currentColumn != mazeSize - 1 && currentRow != mazeSize - 1) {
-//TODO check this, i think that is not correct, but I'm without time to see this now
-        moveOnMaze(maze, moveTo, markAsGenerated, currentColumn, currentRow);
-
-        moveTo = randomNextMove(lastMove, currentColumn, currentRow);       
-	lastMove = moveTo;
-
-    }
+    //initial
+    maze[currentColumn][currentRow] = markAsGenerated;
+    
+    while (!(currentColumn == mazeSize - 1 && currentRow == mazeSize - 1)) {
+        //TODO check this, i think that is not correct, but I'm without time to see this now
+        int moveTo = randomNextMove(maze, lastMove, currentColumn, currentRow);
+        moveOnMaze(maze, moveTo, player, currentColumn, currentRow);
+        lastMove = moveTo;
+		system("cls");
+		printMaze(maze);
+	}
 
     //populating maze
     for (int column = 0; column < mazeSize; column++) {
         for (int row = 0; row < mazeSize; row++) {
             int field = initial;
             if (maze[column][row] != markAsGenerated) {
-                field = generateRandom(2) == 0 ? wall : initial;
+                field = generateRandom(3) > 0 ? wall : initial;
             }
 
             maze[column][row] = field;
         }
     }
-    return maze;
 }
 
-void printMaze(int maze[mazeSize][mazeSize]) {
-    for (int column = 0; column < 10; column++) {
-        for (int row = 0; row < 10; row++) {
-            int field = maze[column][row];
-            cout << (field == wall ? wallChar : field == player ? playerChar : emptyChar);
-        }
-        cout << endl;
-    }
-}
-
-int main() {
+int main()
+{
     int maze[mazeSize][mazeSize];
     generateMaze(maze);
     printMaze(maze);
+
     return 0;
 }
